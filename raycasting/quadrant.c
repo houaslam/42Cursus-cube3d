ad840975ec692b9beebeda20b2fa158b5b32c697
Hajar Ouaslam <houaslam@e1r12p10.1337.ma>
2023-07-30 17:13:32 +0100

    cube

--- raycasting/quadrant.c
+++ raycasting/quadrant.c
@@ -6,7 +6,7 @@
 /*   By: houaslam <houaslam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/15 12:34:20 by houaslam          #+#    #+#             */
-/*   Updated: 2023/07/29 08:31:33 by houaslam         ###   ########.fr       */
+/*   Updated: 2023/07/30 17:12:45 by houaslam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,14 +39,16 @@ void	up_right(t_map *map)
 	map->r.up = 1;
 	map->r.right = 1;
 	map->r.alpha = map->r.cast;
+
 	map->h.y = ((floor(map->p.u_y / UNIT)) * UNIT);
-	map->h.x = map->p.u_x + ((map->p.u_y - map->h.y) \
-	/ tan(map->r.alpha * M_PI / 180));
+	map->h.x = map->p.u_x + ((map->p.u_y - map->h.y) / tan(map->r.alpha * M_PI / 180));
+
 	map->v.x = (floor(map->p.u_x / UNIT) * UNIT) + UNIT;
-	map->v.y = tan(map->r.alpha * (M_PI / 180)) \
-	* (map->p.u_x - map->v.x) + map->p.u_y;
+	map->v.y = tan(map->r.alpha * (M_PI / 180)) * (map->p.u_x - map->v.x) + map->p.u_y;
+
 	map->h.step_y = -UNIT;
 	map->h.step_x = UNIT / tan(map->r.alpha * M_PI / 180);
+
 	map->v.step_x = UNIT;
 	map->v.step_y = -UNIT * tan(map->r.alpha * M_PI / 180);
 }
@@ -56,14 +58,16 @@ void	up_left(t_map *map)
 	map->r.up = 1;
 	map->r.left = 1;
 	map->r.alpha = map->r.cast - 90;
+
 	map->h.y = (floor(map->p.u_y / UNIT) * UNIT);
-	map->h.x = map->p.u_x - ((map->p.u_y - map->h.y) \
-	* tan(map->r.alpha * M_PI / 180));
+	map->h.x = map->p.u_x - ((map->p.u_y - map->h.y) * tan(map->r.alpha * M_PI / 180));
+
 	map->v.x = (floor(map->p.u_x / UNIT) * UNIT);
-	map->v.y = map->p.u_y - ((map->p.u_x - map->v.x) \
-	/ tan(map->r.alpha * M_PI / 180));
+	map->v.y = map->p.u_y - ((map->p.u_x - map->v.x) / tan(map->r.alpha * M_PI / 180));
+
 	map->h.step_y = (-UNIT);
 	map->h.step_x = (-UNIT) * tan(map->r.alpha * M_PI / 180);
+
 	map->v.step_x = (-UNIT);
 	map->v.step_y = (-UNIT) / (tan(map->r.alpha * M_PI / 180));
 }
@@ -73,14 +77,16 @@ void	down_left(t_map *map)
 	map->r.down = 1;
 	map->r.left = 1;
 	map->r.alpha = map->r.cast - 180;
+
 	map->h.y = (floor(map->p.u_y / UNIT) * UNIT) + UNIT;
-	map->h.x = map->p.u_x + (map->p.u_y - map->h.y) \
-	/ tan(map->r.alpha * M_PI / 180);
+	map->h.x = map->p.u_x + (map->p.u_y - map->h.y) / tan(map->r.alpha * M_PI / 180);
+	
 	map->v.x = (floor(map->p.u_x / UNIT) * UNIT);
-	map->v.y = (map->p.u_x - map->v.x) * tan(map->r.alpha \
-	* M_PI / 180) + map->p.u_y;
+	map->v.y = (map->p.u_x - map->v.x) * tan(map->r.alpha * M_PI / 180) + map->p.u_y;
+
 	map->h.step_y = UNIT;
 	map->h.step_x = -UNIT / tan(map->r.alpha * M_PI / 180);
+
 	map->v.step_x = -UNIT;
 	map->v.step_y = UNIT * tan(map->r.alpha * M_PI / 180);
 }
@@ -90,14 +96,16 @@ void	down_right(t_map *map)
 	map->r.down = 1;
 	map->r.right = 1;
 	map->r.alpha = map->r.cast - 270;
+
 	map->h.y = (floor(map->p.u_y / UNIT) * UNIT) + UNIT;
-	map->h.x = map->p.u_x + ((map->h.y - map->p.u_y) \
-	* tan(map->r.alpha * M_PI / 180));
+	map->h.x = map->p.u_x + ((map->h.y - map->p.u_y) * tan(map->r.alpha * M_PI / 180));
+
 	map->v.x = (floor(map->p.u_x / UNIT) * UNIT) + UNIT;
-	map->v.y = (map->v.x - map->p.u_x) / (tan(map->r.alpha \
-	* M_PI / 180)) + map->p.u_y;
+	map->v.y = (map->v.x - map->p.u_x) / (tan(map->r.alpha * M_PI / 180)) + map->p.u_y;
+
 	map->h.step_y = UNIT;
 	map->h.step_x = UNIT * tan(map->r.alpha * (M_PI / 180));
+
 	map->v.step_x = UNIT;
 	map->v.step_y = UNIT / tan(map->r.alpha * (M_PI / 180));
 }
